b248ac8b78c0c6247b0f1e915595d88c5636a6c4
aidyn baigarinov <aidyn.baigarinov@gmail.com>
2020-02-18 18:46:47 +0600

    feat: parse relationsdata

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -18,59 +18,70 @@ type APIurls struct {
 
 var all *APIurls
 
-type ArtistsJSON []struct {
+type ArtistsJSON struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
+	// Locations     string   `json:"locations"`
+	// ConcertDates  string   `json:"concertDates"`
+	Relations     string `json:"relations"`
+	RelationsData RelationJSON
 }
 
+var artists []ArtistsJSON
+
 type LocationsJSON struct {
-	Index []struct {
-		ID        int      `json:"id"`
-		Locations []string `json:"locations"`
-		Dates     string   `json:"dates"`
-	} `json:"index"`
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
 }
 
 type DatesJSON struct {
-	Index []struct {
-		ID    int      `json:"id"`
-		Dates []string `json:"dates"`
-	} `json:"index"`
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
 }
 
 type RelationJSON struct {
-	Index []struct {
-		ID            int                 `json:"id"`
-		DatesLocation map[string][]string `json:"datesLocations"`
-	} `json:"index"`
+	// ID            int                 `json:"id"`
+	DatesLocation map[string][]string `json:"datesLocations"`
 }
 
 func rootHandle(w http.ResponseWriter, r *http.Request) {
 	b := getAPI(all.Artists)
-	var artists *ArtistsJSON
 	json.Unmarshal(b, &artists)
 
 	t, errParse := template.ParseFiles("assets/templates/index.html")
 	if errParse != nil {
-		fmt.Println("errParse")
 		log.Fatal((errParse))
 	}
 	t.Execute(w, &artists)
 }
 
 func artistHandle(w http.ResponseWriter, r *http.Request) {
-	t, errParse := template.ParseFiles("assets/templates/artist.html")
-	if errParse != nil {
-		log.Fatal(errParse)
+	id, err := strconv.Atoi(r.FormValue("artist"))
+	if err != nil {
+		log.Fatal(err)
 	}
-	t.Execute(w, nil)
+
+	for index, v := range artists {
+		if index == id-1 {
+			a := getAPI(v.Relations)
+			var rel RelationJSON
+			json.Unmarshal(a, &rel)
+			v.RelationsData = rel
+			t, errParse := template.ParseFiles("assets/templates/artist.html")
+			if errParse != nil {
+				log.Fatal(errParse)
+			}
+			t.Execute(w, v)
+		} else {
+			// TODO: Handle 404
+		}
+	}
+
 }
 
 func getAPI(url string) []byte {
@@ -92,10 +103,6 @@ func main() {
 	http.HandleFunc("/artist", artistHandle)
 	a := getAPI("https://groupietrackers.herokuapp.com/api")
 	json.Unmarshal(a, &all)
-
-	c := getAPI(all.Relation)
-	var relation *RelationJSON
-	json.Unmarshal(c, &relation)
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	http.HandleFunc("/", rootHandle)
